ad6a6f02dabf68062f839647dcfcda8084aa2564
North-al <63443442+North-al@users.noreply.github.com>
2022-09-27 09:44:27 +0000

    Deploying to gh-pages from @ North-al/vue3-admin-template@0f92eada343b70b21abef96a82729023dc48897b 🚀

---
+++ assets/detail.c6773835.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t}from"./index.2fc3a922.js";const n={};function o(r,a){return t(),c("div",null,"pages1")}const _=e(n,[["render",o]]);export{_ as default};
--- assets/index.718fba28.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index.c9189fbe.js";const o={};function r(t,s){return n(),c("div",null,"page2s")}const _=e(o,[["render",r]]);export{_ as default};
