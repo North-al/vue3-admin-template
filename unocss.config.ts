a9abbe97cdacf2b299e4f1366806320306bc7845
North <1227379879@qq.com>
2022-10-18 11:53:55 +0800

    style: 新增工作台board页面

--- unocss.config.ts
+++ unocss.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
 		'flex-end': 'flex items-center justify-end',
 		'flex-around': 'flex items-center justify-around',
 		'flex-between': 'flex items-center justify-between',
-		'n-hover-gray-500': 'text-gray-500 dark:hover:text-white hover:text-black transition-duration-300'
+		'n-hover-gray-500': 'text-gray-500 dark:hover:text-white hover:text-black transition-duration-300',
+		'dark-theme': 'dark:bg-$dark-bg dark:text-$dark-text',
+		'light-theme': 'bg-$light-bg text-$light-text',
+		theme: 'dark-theme light-theme',
+		'alternate-bg': 'dark:bg-$dark-side bg-$light-side', // 区别于主题色的背景色
+		'alternate-text': 'dark:text-$dark-text text-$light-text' // 区别于正常主题的更深的文字颜色
 	},
 	theme: {}
 })
